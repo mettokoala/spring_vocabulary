ed0f479004b5c2a77fffa5716c0863ead0e8e6a2
yamakoala <52974545+mettokoala@users.noreply.github.com>
2025-04-20 15:42:03 +0900

    Repositoryの作成

--- spring-vocabulary/src/main/java/com/example/demo/domain/entity/Word.java
+++ spring-vocabulary/src/main/java/com/example/demo/domain/entity/Word.java
@@ -6,8 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
---
+++ spring-vocabulary/src/main/java/com/example/demo/domain/repository/WordRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.domain.entity.Word;
+
+@Repository
+public interface WordRepository extends JpaRepository<Word, Long> {
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE Word w SET w.status = false WHERE w.status = true AND w.nextPresentation <= CURRENT_TIMESTAMP")
+	void resertStatus();
+
+}
