9b83030cb73323add13066c6a778981d5aee72e4
yamakoala <52974545+mettokoala@users.noreply.github.com>
2025-04-20 15:38:32 +0900

    Entityの作成

---
+++ spring-vocabulary/src/main/java/com/example/demo/domain/entity/Word.java
@@ -0,0 +1,36 @@
+package com.example.demo.domain.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Word {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String question;
+
+	@Column(nullable = false)
+	private String answer;
+
+	@Column(nullable = false)
+	private boolean status;
+
+	@Column
+	private LocalDateTime nextPresentation;
+
+}
