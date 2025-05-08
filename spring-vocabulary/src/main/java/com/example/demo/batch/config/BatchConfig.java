d3462e01071b2d1d93e687187050cee7a6cae90d
yamakoala <52974545+mettokoala@users.noreply.github.com>
2025-05-08 23:34:49 +0900

    BatchConfigでChunkの設定

--- spring-vocabulary/src/main/java/com/example/demo/batch/config/BatchConfig.java
+++ spring-vocabulary/src/main/java/com/example/demo/batch/config/BatchConfig.java
@@ -9,7 +9,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import com.example.demo.batch.chunk.CsvItemProcessor;
+import com.example.demo.batch.chunk.CsvItemReader;
+import com.example.demo.batch.chunk.CsvItemWriter;
 import com.example.demo.batch.tasklet.ResetTasklet;
+import com.example.demo.domain.entity.Word;
 
 @Configuration
 public class BatchConfig {
@@ -17,21 +21,42 @@ public class BatchConfig {
 	private final JobRepository jobRepository;
 	private final PlatformTransactionManager transactionManager;
 	private final ResetTasklet resetTasklet;
+	private final CsvItemReader csvItemReader;
+	private final CsvItemProcessor csvItemProcessor;
+	private final CsvItemWriter csvItemWriter;
 
 	public BatchConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager,
-			ResetTasklet resetTasklet) {
+			ResetTasklet resetTasklet, CsvItemReader csvItemReader, CsvItemProcessor csvItemProcessor,
+			CsvItemWriter csvItemWriter) {
 		this.jobRepository = jobRepository;
 		this.transactionManager = transactionManager;
 		this.resetTasklet = resetTasklet;
+		this.csvItemReader = csvItemReader;
+		this.csvItemProcessor = csvItemProcessor;
+		this.csvItemWriter = csvItemWriter;
 	}
 
 	@Bean
-	public Job resetTaskletJob() {
+	Job resetTaskletJob() {
 		return new JobBuilder("resetTaskletJob", jobRepository).start(resetTaskletStep()).build();
 	}
 
 	@Bean
-	public Step resetTaskletStep() {
+	Step resetTaskletStep() {
 		return new StepBuilder("resetTaskletStep", jobRepository).tasklet(resetTasklet, transactionManager).build();
+	}
+
+	@Bean
+	Job csvChunkJob() {
+		return new JobBuilder("csvChunkJob", jobRepository).start(csvChunkStep()).build();
+	}
+
+	@Bean
+	Step csvChunkStep() {
+		return new StepBuilder("csvChunkStep", jobRepository).<Word, Word> chunk(10, transactionManager)
+				.reader(csvItemReader)
+				.processor(csvItemProcessor)
+				.writer(csvItemWriter)
+				.build();
 	}
 }
