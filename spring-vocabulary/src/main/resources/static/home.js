4fdb55b90ee2d02c613452f500597fe754de9970
yamakoala <52974545+mettokoala@users.noreply.github.com>
2025-05-24 18:44:50 +0900

    画面とJSの作成2

--- spring-vocabulary/src/main/resources/static/home.js
+++ spring-vocabulary/src/main/resources/static/home.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 	const fileInput = document.getElementById('fileInput');
 	const importCsvButton = document.getElementById('importCsvButton');
 	
-	importCsvButton.addEventListener('click', () =>{
+	importCsvButton.addEventListener('click', async () =>{
 		const file = fileInput.files[0];
 		if (!file){
 			alert('ファイルが選択されていません');
@@ -13,6 +13,31 @@ document.addEventListener('DOMContentLoaded', () =>{
 			alert('CSVファイルを選択してください');
 			return
 		}
+		try{
+			await uploadCsvFile(file);
+			alert('アップロードを行いました');
+			fileInput.value = '';
+		}catch(error){
+			console.error('アップロードエラー',error);
+			alert('アップロードエラー', error);
+		}
+		
 		
 	})
 })
+
+async function uploadCsvFile(file){
+	const url = '/import';
+	const formData = new FormData();
+	formData.append('file', file);
+	
+	const response = await fetch(url, {
+		method: 'POST',
+		body: formData,
+	});
+	
+	if (!response.ok){
+		const errorText = await response.text();
+		throw new Error(errorText || 'サーバーエラー');
+	}
+}
