9db2bbdca86377c461c0cbfb3ee4465efaa06b38
yamakoala <52974545+mettokoala@users.noreply.github.com>
2025-06-15 00:37:55 +0900

    単語データの一覧取得

--- spring-vocabulary/src/main/resources/static/home.js
+++ spring-vocabulary/src/main/resources/static/home.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () =>{
+	getAllWords();
 	const fileInput = document.getElementById('fileInput');
 	const importCsvButton = document.getElementById('importCsvButton');
 	
@@ -40,4 +41,48 @@ async function uploadCsvFile(file){
 		const errorText = await response.text();
 		throw new Error(errorText || 'サーバーエラー');
 	}
+}
+
+async function getAllWords(){
+	try{
+		const response = await fetch('/words');
+		if (!response.ok){
+			const errorText = await response.text();
+			throw new Error(errorText || 'サーバーエラー');
+		}
+		const data = await response.json();
+		createWordsList(data);
+	}catch(error){
+		console.error(error);
+		alert('単語の取得に失敗しました。');
+	}
+}
+
+function createWordsList(words){
+	const wordListElement = document.querySelector('ul');
+	wordListElement.innerHTML = '';
+	
+	words.forEach(word => {
+		const listItem = document.createElement('li');
+		listItem.className = 'word-item';
+		listItem.dataset.wordId = word.id;
+		
+		const toggleButton = document.createElement('button');
+		toggleButton.className = 'toggle-text-button';
+		toggleButton.textContent = word.question;
+		
+		const statusButton = document.createElement('button');
+		statusButton.className = 'status-button';
+		statusButton.textContent = 'OK';
+		
+		const deleteButton = document.createElement('button');
+		deleteButton.className = 'delete-button';
+		deleteButton.textContent = '削除';
+		
+		listItem.appendChild(toggleButton);
+		listItem.appendChild(statusButton);
+		listItem.appendChild(deleteButton);
+		
+		wordListElement.appendChild(listItem);
+	})
 }
